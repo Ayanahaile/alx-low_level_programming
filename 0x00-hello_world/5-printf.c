aa185463f46109823898da6ad9b8a54c5ae2b064
Ayanahaile <getuh775@gmail.com>
2022-09-09 04:15:44 -0700

    commit

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,12 +1,5 @@
 #include <stdio.h>
-/**
-* main  print the string in the put
-*
-*Description: using the main function
-* this progam prints "programming is like a multilingual puzzle.
-* return : 0
-*/
 int main(void)
-{    printf(" with proper grammar, but the outcome is a piece of art,\n");
-    return (0);
+{   printf("with proper grammar, but the outcome is a piece of art,\n);
+   return (0);
 }
