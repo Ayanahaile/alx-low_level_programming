74b06093bb070eee75b482a70911716c21f8e6ec
Ayanahaile <getuh775@gmail.com>
2022-09-10 10:18:13 -0700

    printf function in the c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,5 +1,14 @@
 #include <stdio.h>
+
+/**
+* main - print string in the printf function
+*
+* Description: using main function
+* this program pringt with proper grammar but outcome is piece of code art
+* Return:0
+*/
 int main(void)
-{   printf("with proper grammar, but the outcome is a piece of art,\n);
-   return (0);
+{
+printf("with proper grammar, but the outcome is a piece of art,\n);
+return (0);
 }
