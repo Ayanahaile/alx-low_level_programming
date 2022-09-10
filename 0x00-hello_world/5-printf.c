5beba84e2ccb6c0ee2f990f542219332787033a9
Ayanahaile <getuh775@gmail.com>
2022-09-10 10:47:42 -0700

    printf function

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
 
 /**
-* main -print string in the function
+* main - pring string function
 *
-*Description:using main function
-* this program is pringt programming is like building a multilingual puzzle
-* Return : 0
+* Description:using main function
+* this program pringt Programming is like building a multilingual puzzle
+* Return:0
 */
 int main(void)
 {
-printf("with proper grammar,but the outcome is a piece of art,\n");
+printf("with proper grammar, but the outcome is a piece of art,\n");
 return (0);
 }
