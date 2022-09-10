51da3bfe5c91952f4abf154f4b5109225399efcf
Ayanahaile <getuh775@gmail.com>
2022-09-10 01:58:55 -0700

    code

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,331 @@
 #include <stdio.h>
 /**
-* main - pring string in the puts function
-*
-*description: main function
-* this program prints "programming is like building a multilingual puzzle
-*return:0
-*/
-int main(void)
-{
- puts("\"programming is like building a multilingual puzzle\n");
-return (0);
-}
+ * main - Entry point 
+ *
+ * return 0: After finish success fully
+ * /
+  int main(void)
+  { puts("\" programming is like building a multilingual puzzle");
+        return (0);
+ }
+#include <stdio.h>
+/**
+ * main - Entry point 
+ *
+ * return 0: After finish success fully
+ * /
+  int main(void)
+  { puts("\" programming is like building a multilingual puzzle");
+        return (0);
+ }
+#include <stdio.h>
+/**
+ * main - Entry point 
+ *
+ * return 0: After finish success fully
+ * /
+  int main(void)
+  { puts("\" programming is like building a multilingual puzzle");
+        return (0);
+ }
+#include <stdio.h>
+/**
+ * main - Entry point 
+ *
+ * return 0: After finish success fully
+ * /
+  int main(void)
+  { puts("\" programming is like building a multilingual puzzle");
+        return (0);
+ }
+#include <stdio.h>
+/**
+ * main - Entry point 
+ *
+ * return 0: After finish success fully
+ * /
+  int main(void)
+  { puts("\" programming is like building a multilingual puzzle");
+        return (0);
+ }
+#include <stdio.h>
+/**
+ * main - Entry point 
+ *
+ * return 0: After finish success fully
+ * /
+  int main(void)
+  { puts("\" programming is like building a multilingual puzzle");
+        return (0);
+ }
+#include <stdio.h>
+/**
+ * main - Entry point 
+ *
+ * return 0: After finish success fully
+ * /
+  int main(void)
+  { puts("\" programming is like building a multilingual puzzle");
+        return (0);
+ }
+#include <stdio.h>
+/**
+ * main - Entry point 
+ *
+ * return 0: After finish success fully
+ * /
+  int main(void)
+  { puts("\" programming is like building a multilingual puzzle");
+        return (0);
+ }
+#include <stdio.h>
+/**
+ * main - Entry point 
+ *
+ * return 0: After finish success fully
+ * /
+  int main(void)
+  { puts("\" programming is like building a multilingual puzzle");
+        return (0);
+ }
+#include <stdio.h>
+/**
+ * main - Entry point 
+ *
+ * return 0: After finish success fully
+ * /
+  int main(void)
+  { puts("\" programming is like building a multilingual puzzle");
+        return (0);
+ }
+#include <stdio.h>
+/**
+ * main - Entry point 
+ *
+ * return 0: After finish success fully
+ * /
+  int main(void)
+  { puts("\" programming is like building a multilingual puzzle");
+        return (0);
+ }
+#include <stdio.h>
+/**
+ * main - Entry point 
+ *
+ * return 0: After finish success fully
+ * /
+  int main(void)
+  { puts("\" programming is like building a multilingual puzzle");
+        return (0);
+ }
+#include <stdio.h>
+/**
+ * main - Entry point 
+ *
+ * return 0: After finish success fully
+ * /
+  int main(void)
+  { puts("\" programming is like building a multilingual puzzle");
+        return (0);
+ }
+#include <stdio.h>
+/**
+ * main - Entry point 
+ *
+ * return 0: After finish success fully
+ * /
+  int main(void)
+  { puts("\" programming is like building a multilingual puzzle");
+        return (0);
+ }
+#include <stdio.h>
+/**
+ * main - Entry point 
+ *
+ * return 0: After finish success fully
+ * /
+  int main(void)
+  { puts("\" programming is like building a multilingual puzzle");
+        return (0);
+ }
+#include <stdio.h>
+/**
+ * main - Entry point 
+ *
+ * return 0: After finish success fully
+ * /
+  int main(void)
+  { puts("\" programming is like building a multilingual puzzle");
+        return (0);
+ }
+#include <stdio.h>
+/**
+ * main - Entry point 
+ *
+ * return 0: After finish success fully
+ * /
+  int main(void)
+  { puts("\" programming is like building a multilingual puzzle");
+        return (0);
+ }
+#include <stdio.h>
+/**
+ * main - Entry point 
+ *
+ * return 0: After finish success fully
+ * /
+  int main(void)
+  { puts("\" programming is like building a multilingual puzzle");
+        return (0);
+ }
+#include <stdio.h>
+/**
+ * main - Entry point 
+ *
+ * return 0: After finish success fully
+ * /
+  int main(void)
+  { puts("\" programming is like building a multilingual puzzle");
+        return (0);
+ }
+#include <stdio.h>
+/**
+ * main - Entry point 
+ *
+ * return 0: After finish success fully
+ * /
+  int main(void)
+  { puts("\" programming is like building a multilingual puzzle");
+        return (0);
+ }
+#include <stdio.h>
+/**
+ * main - Entry point 
+ *
+ * return 0: After finish success fully
+ * /
+  int main(void)
+  { puts("\" programming is like building a multilingual puzzle");
+        return (0);
+ }
+#include <stdio.h>
+/**
+ * main - Entry point 
+ *
+ * return 0: After finish success fully
+ * /
+  int main(void)
+  { puts("\" programming is like building a multilingual puzzle");
+        return (0);
+ }
+#include <stdio.h>
+/**
+ * main - Entry point 
+ *
+ * return 0: After finish success fully
+ * /
+  int main(void)
+  { puts("\" programming is like building a multilingual puzzle");
+        return (0);
+ }
+#include <stdio.h>
+/**
+ * main - Entry point 
+ *
+ * return 0: After finish success fully
+ * /
+  int main(void)
+  { puts("\" programming is like building a multilingual puzzle");
+        return (0);
+ }
+#include <stdio.h>
+/**
+ * main - Entry point 
+ *
+ * return 0: After finish success fully
+ * /
+  int main(void)
+  { puts("\" programming is like building a multilingual puzzle");
+        return (0);
+ }
+#include <stdio.h>
+/**
+ * main - Entry point 
+ *
+ * return 0: After finish success fully
+ * /
+  int main(void)
+  { puts("\" programming is like building a multilingual puzzle");
+        return (0);
+ }
+#include <stdio.h>
+/**
+ * main - Entry point 
+ *
+ * return 0: After finish success fully
+ * /
+  int main(void)
+  { puts("\" programming is like building a multilingual puzzle");
+        return (0);
+ }
+#include <stdio.h>
+/**
+ * main - Entry point 
+ *
+ * return 0: After finish success fully
+ * /
+  int main(void)
+  { puts("\" programming is like building a multilingual puzzle");
+        return (0);
+ }
+#include <stdio.h>
+/**
+ * main - Entry point 
+ *
+ * return 0: After finish success fully
+ * /
+  int main(void)
+  { puts("\" programming is like building a multilingual puzzle");
+        return (0);
+ }
+#include <stdio.h>
+/**
+ * main - Entry point 
+ *
+ * return 0: After finish success fully
+ * /
+  int main(void)
+  { puts("\" programming is like building a multilingual puzzle");
+        return (0);
+ }
+#include <stdio.h>
+/**
+ * main - Entry point 
+ *
+ * return 0: After finish success fully
+ * /
+  int main(void)
+  { puts("\" programming is like building a multilingual puzzle");
+        return (0);
+ }
+#include <stdio.h>
+/**
+ * main - Entry point 
+ *
+ * return 0: After finish success fully
+ * /
+  int main(void)
+  { puts("\" programming is like building a multilingual puzzle");
+        return (0);
+ }
+#include <stdio.h>
+/**
+ * main - Entry point 
+ *
+ * return 0: After finish success fully
+ * /
+  int main(void)
+  { puts("\" programming is like building a multilingual puzzle");
+        return (0);
+ }
+
