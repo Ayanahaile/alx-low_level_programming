afcfd878bac5928faf6d745013b8103a24974356
Ayanahaile <getuh775@gmail.com>
2022-09-10 00:48:33 -0700

    use puts function in the c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 /**
-* main  print the string in the put function
-*
-* Description: using the main function
-* this program print "programming is like a multilingual puzzle
-* return:0*/
-int main(void)
-{  puts('\"Programming is like building a multilingual puzzle');
-  return (0);
+ * main - Entry point
+ * 
+ * return: Always 0 (success)
+ */
+ int main(void)
+{ 
+  puts("\"programming is like building a multilingual puzzle");
+ return (0);
 }
