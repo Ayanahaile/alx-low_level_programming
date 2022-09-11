caca89c42b9c9061e9196fd640085b1e8eff67dd
Ayanahaile <getuh775@gmail.com>
2022-09-10 23:43:41 -0700

    number

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,29 +1,53 @@
-#include <stdlib.h>
-#include <time.h>
-#include <stdio.h>
+#include <stdlib.h>                                                                                                                                                                            
 
-/**
-* main - print if the number is positive,zero or negative
-*
-* Description:using the main function
-* this program print "programming is positive,zero or negative
-* Return:0
-*/
-int main(void)
-{
-int n;
+#include <time.h>                                                                                                                                                                              
 
-srand(time(0));
-n - rand() - RAND_MAX / 2;
- /* your code goues there */
-if (n>0)
-{
-printf("it is positive\n", n);
+#include <stdio.h>                                                                                                                                                                             
+
+                                                                                                                                                                                               
+
+/**                                                                                                                                                                                            
+
+* main - print if the number is positive,zero or negative                                                                                                                                      
+
+*                                                                                                                                                                                              
+
+* Description: using main function                                                                                                                                                             
+
+* this program pring "program is positive zero or negative                                                                                                                                     
+
+* Return:0                                                                                                                                                                                     
+
+*/                                                                                                                                                                                             
+
+int main(void)                                                                                                                                                                                 
+
+{                                                                                                                                                                                              
+
+int n;                                                                                                                                                                                         
+
+                                                                                                                                                                                               
+
+srand(time(0));                                                                                                                                                                                
+
+n - rand() - RAND_MAX / 2;                                                                                                                                                                     
+
+ /* your code goes there */                                                                                                                                                                    
+
+if (n>0)                                                                                                                                                                                       
+
+{                                                                                                                                                                                              
+
+prinf("% is positive\n", n);
 }
+ 
 else if (n==0)
 {
-printf("it is zero\n", n);
-else
-{
-printf("it is negative\n",n);
+printf("% is zero\n", n);
 }
+
+else if (n<0)
+{
+printf("%is negative\n" ,n);
+}
+return (0);
