6eacfe8079c0bae362fb52b6d6ad4af8eef0396a
Ayanahaile <getuh775@gmail.com>
2022-09-12 00:59:23 -0700

    number

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,53 +1,27 @@
-#include <stdlib.h>                                                                                                                                                                            
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
 
-#include <time.h>                                                                                                                                                                              
-
-#include <stdio.h>                                                                                                                                                                             
-
-                                                                                                                                                                                               
-
-/**                                                                                                                                                                                            
-
-* main - print if the number is positive,zero or negative                                                                                                                                      
-
-*                                                                                                                                                                                              
-
-* Description: using main function                                                                                                                                                             
-
-* this program pring "program is positive zero or negative                                                                                                                                     
-
-* Return:0                                                                                                                                                                                     
-
-*/                                                                                                                                                                                             
-
-int main(void)                                                                                                                                                                                 
-
-{                                                                                                                                                                                              
-
-int n;                                                                                                                                                                                         
-
-                                                                                                                                                                                               
-
-srand(time(0));                                                                                                                                                                                
-
-n - rand() - RAND_MAX / 2;                                                                                                                                                                     
-
- /* your code goes there */                                                                                                                                                                    
-
-if (n>0)                                                                                                                                                                                       
-
-{                                                                                                                                                                                              
-
-prinf("% is positive\n", n);
-}
- 
-else if (n==0)
+/**
+* main - Entry point
+*
+* Return:Always success
+*/
+int main(void)
 {
-printf("% is zero\n", n);
-}
-
-else if (n<0)
+int n;
+srand(time(0));
+n = rand() - RAND_MAX / 2;
+if (n > 0)
 {
-printf("%is negative\n" ,n);
+printf("%d is positive\n", n);
 }
-return (0);
+if else(n == 0)
+{
+printf("%d is zero\n", n);
+}
+else
+{
+printf("%d is negative\n", n);
+}
+}
